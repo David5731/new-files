4c3ae992bd1a137c7170d0dc2ab1aafb115b1f2a
Anarchy5731 <tloko536@gmail.com>
2024-09-13 09:30:30 +0100

    Delete type shit.py

--- type shit.py
+++
@@ -1,9 +0,0 @@
-num = int(input("number: "))
-newnum = []
-for i in range(1,num):
-    if num % i == 0:
-        newnum.append(i)
-if sum(newnum) == num:
-        print("perfect ")
-else:
-    print("imperfect")
